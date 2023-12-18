05a61bce0e6a5f53e1a7b693e09ee87869bf7843
Miguel Santos <santosmiguel25@gmail.com>
2023-12-18 19:41:46 +0000

    Push solution for day 4 part 2

--- 2023/Day4/solution.ts
+++ 2023/Day4/solution.ts
@@ -6,12 +6,13 @@ interface ScratchCard {
   winningNumbers: number[];
   myNumbers: number[];
   matchedNumbers: number[];
+  isCopy: boolean;
+  cardIndex: number;
+  isProcessed: boolean;
 }
 
-function getSolution1(inputLines: string[]) {
-  let sol = 0;
-
-  const scratchCards = inputLines.map((line, i) => {
+function parseScratchCards(inputLines: string[]): ScratchCard[] {
+  return inputLines.map((line, i) => {
     const card = line.split('|');
     const winningNumbers = card[0].split(':')[1].trim().split(' ').map(n => parseInt(n));
     const myNumbers = card[1].trim().split(' ').filter(Boolean).map(n => parseInt(n));
@@ -19,9 +20,18 @@ function getSolution1(inputLines: string[]) {
     return {
       winningNumbers,
       myNumbers,
-      matchedNumbers: myNumbers.filter(n => winningNumbers.includes(n))
+      matchedNumbers: myNumbers.filter(n => winningNumbers.includes(n)),
+      isCopy: false,
+      cardIndex: i,
+      isProcessed: false,
     };
   });
+}
+
+function getSolution1(inputLines: string[]) {
+  let sol = 0;
+
+  const scratchCards = parseScratchCards(inputLines);
 
   scratchCards
     .filter(card => card.matchedNumbers.length > 0)
@@ -35,7 +45,32 @@ function getSolution1(inputLines: string[]) {
 }
 
 function getSolution2(inputLines: string[]) {
-  return 0;
+  let sol = 0;
+
+  const scratchCards = parseScratchCards(inputLines);
+
+  for (let i = 0; i < scratchCards.length; i++) {
+    const card = scratchCards[i];
+
+    if (card.matchedNumbers.length > 0) {
+      // Copy scratch card from index i by length of matchedNumbers
+      for (let j = card.cardIndex + 1; j < card.cardIndex + 1 + card.matchedNumbers.length; j++) {
+        if (scratchCards[j].isCopy) {
+          scratchCards[j].isProcessed = true;
+        }
+
+        if (scratchCards[j].isProcessed) {
+          continue;
+        }
+
+        scratchCards.push({ ...scratchCards[j], isCopy: true });
+      }
+    }
+  }
+
+  sol = scratchCards.length;
+
+  return sol;
 }
 
 // Part 1
