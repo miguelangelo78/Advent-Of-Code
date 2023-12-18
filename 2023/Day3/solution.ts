95d7eaa4aea617557e8e8cba9d3ce0c07f10512e
Miguel Santos <santosmiguel25@gmail.com>
2023-12-18 17:43:50 +0000

    Push solution day 3 part 2

--- 2023/Day3/solution.ts
+++ 2023/Day3/solution.ts
@@ -9,7 +9,7 @@ interface Token {
   length: number;
 }
 
-function tokenise(inputLine: string, y: number): Token[] {
+function tokeniseLine(inputLine: string, y: number): Token[] {
   const tokens = [];
 
   const chars = inputLine.trim().split('');
@@ -33,14 +33,21 @@ function tokenise(inputLine: string, y: number): Token[] {
   return tokens;
 }
 
-function getSolution1(inputLines: string[]) {
-  let sol = 0;
+function tokeniseAllLines(inputLines: string[]): Token[] {
   const tokens = [];
 
-  // Tokenise the input into a list of tokens
   for (let y = 0; y < inputLines.length; y++) {
-    tokens.push(...tokenise(inputLines[y], y));
+    tokens.push(...tokeniseLine(inputLines[y], y));
   }
+
+  return tokens;
+}
+
+function getSolution1(inputLines: string[]) {
+  let sol = 0;
+
+  // Tokenise the input into a list of tokens
+  const tokens = tokeniseAllLines(inputLines);
 
   const symbolTokens = tokens.filter((token) => isNaN(+token.char));
   const numberTokens = tokens.filter((token) => !isNaN(+token.char));
@@ -73,10 +80,49 @@ function getSolution1(inputLines: string[]) {
   return sol;
 }
 
+function getSolution2(inputLines: string[]) {
+  const tokens = tokeniseAllLines(inputLines);
+
+  const gearTokens = tokens.filter((token) => token.char === '*');
+  const numberTokens = tokens.filter((token) => !isNaN(+token.char));
+
+  let sol = 0;
+
+  for (let i = 0; i < gearTokens.length; i++) {
+    const gearToken = gearTokens[i];
+
+    let adjacentNumbers = [];
+
+    for (let j = 0; j < numberTokens.length; j++) {
+      const numberToken = numberTokens[j];
+
+      let xDistance, yDistance;
+
+      if (numberToken.x >= gearToken.x) {
+        xDistance = Math.abs(numberToken.x - gearToken.x);
+      } else {
+        xDistance = Math.abs((numberToken.x + numberToken.length - 1) - gearToken.x);
+      }
+
+      yDistance = Math.abs(numberToken.y - gearToken.y);
+
+      if (xDistance <= 1 && yDistance <= 1) {
+        adjacentNumbers.push(numberToken);
+        if (adjacentNumbers.length === 2) {
+          sol += adjacentNumbers.reduce((acc, curr) => acc * +curr.char, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  return sol;
+}
+
 // Part 1
 const solution1 = getSolution1(inputLines);
 console.log(`Solution part 1: ${solution1}`);
 
 // Part 2
-//const solution2 = getSolution2(inputLines);
-//console.log(`Solution part 2: ${solution2}`);
+const solution2 = getSolution2(inputLines);
+console.log(`Solution part 2: ${solution2}`);
