b2fb681484a256a0342d292d2e6147d298e5ff38
Miguel Santos <santosmiguel25@gmail.com>
2023-12-30 19:04:04 +0000

    Push solution for day 9 part 1

---
+++ 2023/Day9/solution.ts
@@ -0,0 +1,60 @@
+import * as fs from 'fs';
+
+const inputLines = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+function getCoeffs(sensor: number[]) {
+  const listCoeffs = [];
+  let coeffs: number[] = [];
+
+  let lastSensors = [...sensor];
+
+  while (true) {
+    // Get differences between each number:
+    for (let i = 0; i < lastSensors.length - 1; i++) {
+      coeffs.push(lastSensors[i + 1] - lastSensors[i]);
+    }
+
+    listCoeffs.push([...coeffs]);
+
+    if (coeffs.length > 0 && coeffs.every((coeff) => coeff === 0)) {
+      // We've reached the end of the list
+      break;
+    }
+
+    lastSensors = [...coeffs];
+
+    coeffs = [];
+  }
+
+  return listCoeffs;
+}
+
+function getSolution1(inputLines: string[]) {
+  let sol = 0;
+
+  const sensors = inputLines.map((line) => line.trim().split(' ').map((char) => +char));
+
+  const listCoeffs = sensors.map((sensor) => getCoeffs(sensor));
+
+  const extrapolated = listCoeffs.map((coeffs) => coeffs.reduce((acc, coeff) => acc + coeff.at(-1)!, 0));
+
+  sol = sensors.map((sensor) => sensor.at(-1)!).reduce((acc, sensor, index) => acc + sensor + extrapolated[index], 0);
+
+  return sol;
+}
+
+function getSolution2(inputLines: string[]) {
+  let sol = 0;
+
+
+
+  return sol;
+}
+
+// Part 1
+const solution1 = getSolution1(inputLines);
+console.log(`Solution part 1: ${solution1}`);
+
+// Part 2
+const solution2 = getSolution2(inputLines);
+console.log(`Solution part 2: ${solution2}`);
